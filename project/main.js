0950d4e2bbebbf1e9759781aa3ec7346f9dbebd8
Amar <108946547+AmarVcode@users.noreply.github.com>
2023-01-10 15:36:13 -0500

    updated

---
+++ project/main.js
@@ -0,0 +1,19 @@
+function showmenu(){
+    let drop = document.getElementById("navbar")
+if(drop.classList.contains("aftermenu")){
+    drop.classList.remove("aftermenu")
+}
+else{
+    drop.classList.add("aftermenu")
+
+}
+}
+
+
+
+
+
+
+
+
+
